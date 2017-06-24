935e2d9a4bd1aa473200599d97d3419ef52e05cb
playniuniu <playniuniu@gmail.com>
2017-06-24 22:18:05 +0800

    change some websocket js file

--- web/app/static/assets/js/docker_run.js
+++ web/app/static/assets/js/docker_run.js
@@ -1,7 +1,30 @@
 var init_terminal = function() {
     var term = new Terminal();
-    term.open(document.getElementById('#terminal'));
-    term.write('Hello from \033[1;3;31mxterm.js\033[0m $ ')
+    term.open(document.getElementById('terminal'));
+    term.fit();
+    init_websocket(term);
+}
+
+var init_websocket = function(term) {
+    var ws_url = "ws://" + window.location.host + "/ws/docker"
+    // var ws_url = "ws://127.0.0.1:8001/docker";
+    var ws = new WebSocket(ws_url);
+
+    ws.onopen = function(event) {
+        console.log("Connect to " + ws_url);
+    }
+
+    ws.onclose = function(event) {
+        console.log("close websocket");
+        term.detach();
+    }
+
+    ws.onerror = function(event) {
+        console.log("websocket has error!");
+        term.detach();
+    }
+
+    term.attach(ws);
 }
 
 $(document).ready(function () {
